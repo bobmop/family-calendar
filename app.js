bed6b0504590918766aaf7aa8af647efbf6160c7
Matthias Sauer <code@bobmop.de>
2015-10-10 23:21:52 +0200

    added check for existing client credentials before starting the server
    ...based on googl's quickstart for node

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-var express = require('express'),
-	app = express();
+var credentials = require('./app/credentials'),
+	express = require('express'),
+	app = express(),
+	auth;
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000);
+function startServer(auth) {
+	auth = auth;
+	app.listen(process.env.PORT || 3000);
+}
+
+credentials(startServer);
