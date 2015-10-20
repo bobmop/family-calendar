f068cb49fc61fa77b176e9bac9673d94a24079c4
Matthias Sauer <code@bobmop.de>
2015-10-20 09:54:41 +0200

    Added simple access log

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var credentials = require('./app/credentials'),
 	express = require('express'),
+	fs = require('fs'),
+	morgan = require('morgan'),
 	app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+// logging
+var logToDir = __dirname + '/logs/';
+try {
+	fs.mkdirSync(logToDir);
+} catch(e) {
+	if (e.code != 'EEXIST') {
+		throw e;
+	}
+}
+var accessLogStream = fs.createWriteStream(logToDir + '/access.log', {flags: 'a'})
+app.use(morgan('combined', {stream: accessLogStream}));
 
 function startServer(auth) {
 	app.set('google_auth', auth);
