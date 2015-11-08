645d8f67a2b6081b216df2f8cfd015251d5bb4be
Matthias Sauer <code@bobmop.de>
2015-11-08 22:19:36 +0100

    fetch events for every calendar and add them to an events array

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 (function() {
 
-    var calendars = {};
+    var calendars = {},
+        events = [];
 
 
     function fetchCalendars() {
@@ -17,11 +18,40 @@
         });
     }
 
+    function fetchEvents(calendar) {
+        return new Promise(function(fulfill, reject) {
+            var request = new XMLHttpRequest();
+            request.addEventListener("load", function() {
+                fulfill(JSON.parse(this.responseText));
+            });
+            request.addEventListener("error", function() {
+                reject();
+            });
+            request.open("GET", "/events/" + calendar.id);
+            request.send();
+        });
+    }
+
+    function computeEvents(events) {
+        //
+    }
+
     fetchCalendars().then(
         function(result) {
             result.items.forEach(function(item) {
                 calendars[item.id] = item;
             })
+            Object.keys(calendars).reduce(function(sequence, calendar_id) {
+                return sequence.then(function() {
+                    return fetchEvents(calendars[calendar_id]);
+                }, function() {
+                    // initialValue is resolved, errors could be retrieved by
+                    // fetching events from server
+                    console.log("error fetching events for " + calendar_id);
+                }).then(function(eventsObject) {
+                    computeEvents(eventsObject.items);
+                });
+            }, Promise.resolve());
         },
         function() {
             console.log("error while fetching calendars");
