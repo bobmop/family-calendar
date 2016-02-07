f5fc74ded8bf680fff572db6bfc712fdc22e7d61
Matthias Sauer <code@bobmop.de>
2016-02-07 23:26:07 +0100

    replaced tabs by spaces

--- app/credentials.js
+++ app/credentials.js
@@ -1,12 +1,12 @@
 var fs = require('fs'),
-	readline = require('readline'),
-	google = require('googleapis'),
-	googleAuth = require('google-auth-library'),
+    readline = require('readline'),
+    google = require('googleapis'),
+    googleAuth = require('google-auth-library'),
 
-	SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'],
-	TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-		process.env.USERPROFILE) + '/.credentials/',
-	TOKEN_PATH = TOKEN_DIR + 'familiy-calendar-google.json';
+    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'],
+    TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+        process.env.USERPROFILE) + '/.credentials/',
+    TOKEN_PATH = TOKEN_DIR + 'familiy-calendar-google.json';
 
 
 /**
@@ -17,44 +17,44 @@ var fs = require('fs'),
  */
 module.exports = function(callback) {
 
-	/**
-	 * Get and store new token after prompting for user authorization, and then
-	 * execute the given callback with the authorized OAuth2 client.
-	 *
-	 * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
-	 * @param {getEventsCallback} callback The callback to call with the authorized
-	 *	 client.
-	 */
-	function getNewToken(oauth2Client, callback) {
+    /**
+     * Get and store new token after prompting for user authorization, and then
+     * execute the given callback with the authorized OAuth2 client.
+     *
+     * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+     * @param {getEventsCallback} callback The callback to call with the authorized
+     *     client.
+     */
+    function getNewToken(oauth2Client, callback) {
         var authUrl = oauth2Client.generateAuthUrl({
             access_type: 'offline',
-    		scope: SCOPES
-	   });
-    	console.log('Authorize this app by visiting this url: ', authUrl);
-    	var rl = readline.createInterface({
+            scope: SCOPES
+       });
+        console.log('Authorize this app by visiting this url: ', authUrl);
+        var rl = readline.createInterface({
             input: process.stdin,
-    		output: process.stdout
-    	});
-    	rl.question('Enter the code from that page here: ', function(code) {
-    		rl.close();
-    		oauth2Client.getToken(code, function(err, token) {
+            output: process.stdout
+        });
+        rl.question('Enter the code from that page here: ', function(code) {
+            rl.close();
+            oauth2Client.getToken(code, function(err, token) {
                 if (err) {
                     console.log('Error while trying to retrieve access token', err);
                     return;
                 }
                 oauth2Client.credentials = token;
                 storeToken(token);
-        		callback(oauth2Client);
+                callback(oauth2Client);
             });
         });
     }
 
-	/**
-	 * Store token to disk be used in later program executions.
-	 *
-	 * @param {Object} token The token to store to disk.
-	 */
-	function storeToken(token) {
+    /**
+     * Store token to disk be used in later program executions.
+     *
+     * @param {Object} token The token to store to disk.
+     */
+    function storeToken(token) {
         try {
             fs.mkdirSync(TOKEN_DIR);
         } catch (err) {
@@ -66,16 +66,16 @@ module.exports = function(callback) {
         console.log('Token stored to ' + TOKEN_PATH);
     }
 
-	/**
-	 * Create an OAuth2 client with the given credentials, and then execute the
-	 * given callback function.
-	 *
-	 * @param {Object} credentials The authorization client credentials.
-	 * @param {function} callback The callback to call with the autho else {
-		console.log('Found')
-	}rized client.
-	 */
-	function authorize(credentials, callback) {
+    /**
+     * Create an OAuth2 client with the given credentials, and then execute the
+     * given callback function.
+     *
+     * @param {Object} credentials The authorization client credentials.
+     * @param {function} callback The callback to call with the autho else {
+        console.log('Found')
+    }rized client.
+     */
+    function authorize(credentials, callback) {
         var clientSecret = credentials.installed.client_secret,
             clientId = credentials.installed.client_id,
             redirectUrl = credentials.installed.redirect_uris[0],
@@ -92,10 +92,10 @@ module.exports = function(callback) {
                 callback(oauth2Client);
             }
         });
-	}
+    }
 
-	// Load client secrets from a local file.
-	fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+    // Load client secrets from a local file.
+    fs.readFile('client_secret.json', function processClientSecrets(err, content) {
         if (err) {
             console.log('Error loading client secret file: ' + err);
             return;
